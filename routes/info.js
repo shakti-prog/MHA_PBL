4819633b2fbab5b357951b42d8c1699659c91478
Shakti  Tripathi <shaktitripathi02@gmail.com>
2021-11-17 13:13:38 +0530

    Updated

--- routes/info.js
+++ routes/info.js
@@ -5,18 +5,24 @@ const Assets = require('../models/asset');
 const assetRouter = express.Router();
 
 const data1 = [
-  {quantity:10,price:100},{quantity:5,price:250},{quantity:7,price:120},{quantity:6,price:90},{quantity:4,price:75},
-  {quantity:15,price:50},{quantity:13,price:89},{quantity:8,price:78},{quantity:10,price:100},{quantity:11,price:68}
+  {quantity:10,price:100,date:"12/12/2021"},{quantity:5,price:250,date:"6/12/2021"},{quantity:7,price:120,date:"1/12/2021"},
+  {quantity:6,price:90,date:"11/11/2021"},{quantity:4,price:75,date:"9/11/2021"},{quantity:15,price:50,date:"5/11/2021"},
+  {quantity:13,price:89,date:"3/10/2021"},{quantity:8,price:78,date:"1/10/2021"},{quantity:10,price:100,date:"17/10/2021"},
+  {quantity:11,price:68,date:"2/9/2021"},{quantity:17,price:54,date:"21/9/2021"},{quantity:14,price:34,date:"13/9/2021"}
 ];
 
 const data2 = [
-  {quantity:10,Requests:2},{quantity:5,Requests:25},{quantity:7,Requests:14},{quantity:6,Requests:9},{quantity:4,Requests:23},
-  {quantity:15,Requests:1},{quantity:13,Requests:3},{quantity:8,Requests:11},{quantity:10,Requests:4},{quantity:11,Requests:3}
+  {quantity:10,Requests:2,date:"12/12/2021"},{quantity:5,Requests:25,date:"6/12/2021"},{quantity:7,Requests:14,date:"1/12/2021"},
+  {quantity:6,Requests:9,date:"11/11/2021"},{quantity:4,Requests:23,date:"9/11/2021"}, {quantity:15,Requests:1,date:"5/11/2021"},
+  {quantity:13,Requests:3,date:"3/10/2021"},{quantity:8,Requests:11,date:"21/9/2021"},{quantity:10,Requests:4,date:"13/9/2021"},
+  {quantity:12,Requests:4,date:"2/9/2021"},{quantity:8,Requests:9,date:"21/9/2021"},{quantity:4,Requests:19,date:"13/9/2021"}
 ];
 
 const data3 = [
-  {price:100,Requests:2},{price:250,Requests:25},{price:120,Requests:14},{price:90,Requests:9},{price:75,Requests:23},
-  {price:50,Requests:1},{price:89,Requests:3},{price:78,Requests:11},{price:100,Requests:4},{price:68,Requests:3}
+  {price:100,Requests:2,date:"12/12/2021"},{price:250,Requests:25,date:"6/12/2021"},{price:120,Requests:14,date:"1/12/2021"},
+  {price:90,Requests:9,date:"11/11/2021"},{price:75,Requests:23,date:"9/11/2021"},{price:50,Requests:1,date:"5/11/2021"},
+  {price:89,Requests:3,date:"3/10/2021"},{price:78,Requests:11,date:"21/9/2021"},{price:100,Requests:4,date:"13/9/2021"},
+  {price:68,Requests:3,date:"2/9/2021"},{price:56,Requests:14,date:"21/9/2021"},{price:78,Requests:18,date:"13/9/2021"}
 ];
 
 assetRouter.route('/').
